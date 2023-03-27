aa91a46f626c89cf51ef39b8254966cfce2b7f46
bijoymamud <bijoymamud.09@gmail.com>
2023-03-27 23:12:16 +0600

    adding active order summary

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,20 +8,26 @@ const Cart = ({ cart }) => {
     // const { cart } = props;
     console.log(cart);
 
-    let total = 0;
+    let totalPrice = 0;
+    let totalShipping = 0;
+
     for (const product of cart) {
-        total = total + product.price;
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+
+    const tax = totalPrice * 7 / 100;
+    const grandTotal = totalPrice + totalShipping + tax;
 
     return (
         <div className='cart'>
             <h3>Order Summary</h3>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: ${total} </p>
-            <p>Total Shipping: </p>
-            <p>Tax:</p>
-            <p>Grand Total: </p>
-        </div>
+            <p>Total Price: ${totalPrice} </p>
+            <p>Shipping Cost: ${totalShipping} </p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+        </div >
     );
 };
 

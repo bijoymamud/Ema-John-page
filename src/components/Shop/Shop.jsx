ba1a23232c566073eff39f79dc0885a5694bb17a
bijoymamud <bijoymamud.09@gmail.com>
2023-03-28 13:09:18 +0600

    save card to local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -16,6 +17,14 @@ const Shop = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
 
+    }, []);
+
+
+    //for loading data from outsite in DB
+
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
     }, [])
 
 
@@ -24,6 +33,8 @@ const Shop = () => {
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
         setCarts(newCart);
+
+        addToDb(product.id)
     }
 
 
